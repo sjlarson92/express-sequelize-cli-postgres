32cceb29ff398d175c5512bb17cf5c724b7aca87
sasha <sjlarson92@gmail.com>
2025-03-31 00:08:28 -0400

    update config to use env vars

--- config/config.js
+++ config/config.js
@@ -1,10 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config()
+
 const config = {
-    "development": {
-        "username": "sashalarson",
-        "password": "password",
-        "database": "todo_express_sequelizecli",
-        "host": "127.0.0.1",
-        "dialect": "postgres"
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: "postgres"
     }
 }
 
